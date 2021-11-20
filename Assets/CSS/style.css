679fde1f531ee827a7d72f8044bc36f2ef037fcf
edavis56 <92826817+edavis56@users.noreply.github.com>
2021-11-20 03:22:53 -0500

    Intial setup of git repo. Along with the HTML layout and basic CSS styling for each question card

---
+++ Assets/CSS/style.css
@@ -0,0 +1,47 @@
+
+.card {
+  box-shadow: 0 4px 8px 0 rgba(0,0,0,0.2);
+  transition: 0.3s;
+  margin-left: 300;
+  margin-right: 300;
+  margin-top: 200px;
+  justify-content: center;
+  padding-bottom: 10px;
+  }
+
+#cqc {
+  text-align: center;
+  padding: 2px 16px;
+  }
+
+  #q {
+    padding: 2px 16px 2px 70px;
+    }
+
+.pcontainer {
+  margin-left: 100px;
+  margin-right: 100px;
+  padding: 2px 16px;
+  
+}
+
+.button {
+  background-color: blueviolet;
+  color: white;
+  margin-right: auto; 
+  margin-left: 0px;
+  display: block;
+  margin-left: 100px;
+  margin-bottom: 10px;
+  border-radius: 15px;
+  
+}
+
+.start{
+  margin-left: 350px;
+  margin-right: 350px;
+  margin-bottom: 10px;
+  background-color: blueviolet;
+  color: white;
+  border-radius: 15px;
+}
