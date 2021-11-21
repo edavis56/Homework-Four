1c21dfa39f9d48f3b51da1dad68caf6294ad6dd2
edavis56 <92826817+edavis56@users.noreply.github.com>
2021-11-21 17:06:41 -0500

    Update to cards; html files added for each one

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,47 +1,14 @@
-
-.card {
-  box-shadow: 0 4px 8px 0 rgba(0,0,0,0.2);
-  transition: 0.3s;
-  margin-left: 300;
-  margin-right: 300;
-  margin-top: 200px;
-  justify-content: center;
-  padding-bottom: 10px;
-  }
-
-#cqc {
-  text-align: center;
-  padding: 2px 16px;
-  }
-
-  #q {
-    padding: 2px 16px 2px 70px;
-    }
-
-.pcontainer {
-  margin-left: 100px;
-  margin-right: 100px;
-  padding: 2px 16px;
-  
+function wrong(){
+alert("Wrong")
 }
 
-.button {
-  background-color: blueviolet;
-  color: white;
-  margin-right: auto; 
-  margin-left: 0px;
-  display: block;
-  margin-left: 100px;
-  margin-bottom: 10px;
-  border-radius: 15px;
-  
-}
+const button = document.getElementById("button");
 
-.start{
-  margin-left: 350px;
-  margin-right: 350px;
-  margin-bottom: 10px;
-  background-color: blueviolet;
-  color: white;
-  border-radius: 15px;
+for (var i = 0; i<7; i++) {
+    const button = document.createElement("button");
+    button.innerText = i;
+
+    button.addEventListener("click", function() {
+        console.log(i)
+    })
 }
